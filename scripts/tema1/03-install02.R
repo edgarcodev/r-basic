39b0a4beb8bc5c9725d2c009f10e12683b9a078b
Edgar21 <codeturas@gmail.com>
2024-09-05 20:30:32 -0500

    prueba2

---
+++ scripts/tema1/03-install02.R
@@ -0,0 +1,13 @@
+install.packages("tidyverse", dep = TRUE)
+
+library(tidyverse)
+
+install.packages("magic", dep = TRUE)
+
+library(magic)
+
+install.packages("abind", dep = TRUE)
+
+library(magic)
+
+magic(6)
